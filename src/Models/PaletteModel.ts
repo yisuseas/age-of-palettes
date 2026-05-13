3e1567d819aab5b609f7aeca598e19ca2a4e220c
yisuseas <yisuseas@gmail.com>
2026-05-12 19:41:09 -0600

    Models

---
+++ src/Models/PaletteModel.ts
@@ -0,0 +1,55 @@
+import type { PlayerName } from "../types";
+import { PlayerModel } from "./PlayerModel";
+import { ALL_PLAYER_NAMES } from "../constant";
+
+const SEARCH_KEY = "palette";
+
+const PLAYER_SEPARATOR = "-";
+
+const VALID_PATTERN = /^([\da-f]{6}-){8}[\da-f]{6}$/i;
+
+export class PaletteModel {
+	playerData: Record<PlayerName, PlayerModel>;
+
+	constructor() {
+		const search = new URLSearchParams(window.location.search);
+		const urlPalette = search.get(SEARCH_KEY);
+		let generatePlayerColors: (
+			name: PlayerName,
+			idx: number
+		) => [PlayerName, PlayerModel];
+
+		if (urlPalette && VALID_PATTERN.test(urlPalette)) {
+			const hexColors = urlPalette.split(PLAYER_SEPARATOR);
+			generatePlayerColors = (name, idx) => {
+				const player = new PlayerModel(hexColors[idx]);
+				return [name, player];
+			};
+		} else {
+			generatePlayerColors = (name, _) => {
+				const player = PlayerModel.fromDefault(name);
+				return [name, player];
+			};
+		}
+
+		this.playerData = Object.fromEntries(
+			ALL_PLAYER_NAMES.map(generatePlayerColors)
+		) as Record<PlayerName, PlayerModel>;
+	}
+
+	updateURL() {
+		const searchValue = ALL_PLAYER_NAMES.map((name) =>
+			this.playerData[name].hexString()
+		).join(PLAYER_SEPARATOR);
+		const url = new URL(window.location.toString());
+		url.searchParams.set(SEARCH_KEY, searchValue);
+		history.replaceState(null, "", url);
+	}
+
+	getAll(): [PlayerName, string][] {
+		return ALL_PLAYER_NAMES.map((name) => [
+			name,
+			this.playerData[name].cssProps(),
+		]);
+	}
+}
---
+++ src/Models/PlayerModel.ts
@@ -0,0 +1,45 @@
+import type { ColorInstance } from "color";
+import Color from "color";
+import type { PlayerName } from "../types";
+import defaultColors from "../spritecolors.json";
+
+const WHITE = new Color("white").toString();
+const BLACK = new Color("black").toString();
+
+export class PlayerModel {
+	unit: ColorInstance;
+
+	constructor(hexStr: string) {
+		this.unit = new Color(`#${hexStr}`);
+	}
+
+	static fromDefault(name: PlayerName) {
+		const original = defaultColors.TeamColors[name].FloatRGBA;
+		const color = Color.rgb(
+			Math.round(original.r * 255),
+			Math.round(original.g * 255),
+			Math.round(original.b * 255)
+		);
+		return new PlayerModel(color.hex().replace("#", ""));
+	}
+
+	background() {
+		return this.unit.toString();
+	}
+
+	foreground() {
+		return this.unit.isLight() ? BLACK : WHITE;
+	}
+
+	hexString() {
+		return this.unit.hex().replace("#", "");
+	}
+
+	set(unit: ColorInstance) {
+		this.unit = unit;
+	}
+
+	cssProps() {
+		return `--bg:${this.background()};--fg:${this.foreground()};`;
+	}
+}
